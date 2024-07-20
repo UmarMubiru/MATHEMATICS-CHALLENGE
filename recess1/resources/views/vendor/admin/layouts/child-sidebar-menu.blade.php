c71f4d1864205777df7859ea3a353993f41a8aae
Arok Rokie <104581401+Arokrokie@users.noreply.github.com>
2024-07-20 16:55:05 +0300

    Delete recess1 directory

--- recess1/resources/views/vendor/admin/layouts/child-sidebar-menu.blade.php
+++
@@ -1,5 +0,0 @@
-<li class="list-divider"></li>
-<li class="nav-small-cap"><span class="hide-menu">{{ __('NMC Dashboard') }}</span></li>
-@foreach(\EasyPanel\Models\CRUD::active() as $crud)
-    <x-easypanel::crud-menu-item :crud="$crud" />
-@endforeach
